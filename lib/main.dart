0e49f49991df4bc25f2f6c0a0da3baab18da7a9d
Thng Yu Xuan <thngyuxuan@gmail.com>
2022-05-23 11:05:43 +0800

    Add top bar

--- lib/main.dart
+++ lib/main.dart
@@ -24,13 +24,28 @@ class MyApp extends StatelessWidget {
         // is not restarted.
         primarySwatch: Colors.blue,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      routes: {
+        '/': (context) => const MyHomePage(),
+      },
+    );
+  }
+}
+
+class WelcomeScreen extends StatelessWidget {
+  const WelcomeScreen();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Text('Welcome!', style: Theme.of(context).textTheme.headline2),
+      ),
     );
   }
 }
 
 class MyHomePage extends StatefulWidget {
-  const MyHomePage({Key? key, required this.title}) : super(key: key);
+  const MyHomePage();
 
   // This widget is the home page of your application. It is stateful, meaning
   // that it has a State object (defined below) that contains fields that affect
@@ -40,8 +55,6 @@ class MyHomePage extends StatefulWidget {
   // case the title) provided by the parent (in this case the App widget) and
   // used by the build method of the State. Fields in a Widget subclass are
   // always marked "final".
-
-  final String title;
 
   @override
   State<MyHomePage> createState() => _MyHomePageState();
@@ -69,11 +82,33 @@ class _MyHomePageState extends State<MyHomePage> {
     // The Flutter framework has been optimized to make rerunning build methods
     // fast, so that you can just rebuild anything that needs updating rather
     // than having to individually change instances of widgets.
+
+    final ButtonStyle style = TextButton.styleFrom(
+        primary: Theme.of(context).colorScheme.onPrimary,
+        padding: const EdgeInsets.all(20));
+
     return Scaffold(
       appBar: AppBar(
         // Here we take the value from the MyHomePage object that was created by
         // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
+        title: InkWell(
+          onTap: () {
+            // route to homepage
+          },
+          child: const Text('NCL Academy'),
+        ),
+        actions: <Widget>[
+          TextButton(
+            style: style,
+            onPressed: () {},
+            child: const Text('NCL Website'),
+          ),
+          TextButton(
+            style: style,
+            onPressed: () {},
+            child: const Text('Help'),
+          )
+        ],
       ),
       body: Center(
         // Center is a layout widget. It takes a single child and positions it
